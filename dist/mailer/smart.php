9985e48a823c15851caebaab14b396e92f5fadaf
Karen <darkphenix@bk.ru>
2020-04-19 03:58:50 +0500

    mailer

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -4,28 +4,30 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 
-use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
+
 require 'src/Exception.php';
 require 'src/PHPMailer.php';
 require 'src/SMTP.php';
 
-$mail = new PHPMailer();
-$mail->CharSet = 'utf-8';
 
-//$mail->SMTPDebug = 1;                               // Enable verbose debug output
+$mail = new PHPMailer;
+//$mail->CharSet = 'utf-8';
+
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                                // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'blackhort';                 // Наш логин
+$mail->Username = 'darkphenix@bk.ru';                 // Наш логин
 $mail->Password = 'Pheonix335359';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465 ;                                    // TCP port to connect to
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('blackhort@yandex.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('darkphenix@bk.ru');     // Add a recipient
+$mail->setFrom('darkphenix@bk.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('blackhort@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -41,10 +43,10 @@ $mail->Body    = '
 	Номер телефона: ' . $phone . '<br>
 	E-mail: ' . $email . '';
 
-if($mail->send()) {
-	echo 'Отправлено!';
+if(!$mail->send()) {
+    return false;
 } else {
-    echo 'Не отправлено!' . $mail->ErrorInfo;;
+    return true;
 }
 
 ?>
